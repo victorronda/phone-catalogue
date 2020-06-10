08046e04094eb402c2683f1fea14635d2925e800
Víctor Ronda <victor.ronda@gmail.com>
2020-06-10 10:26:58 +0200

    styling done

--- catalogue-app/src/components/Header/Header.js
+++ catalogue-app/src/components/Header/Header.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
 	return (
-		<Link className="text-decoration-none hover-title" to="/">
+		<Link className="text-decoration-none" to="/">
 			<div className="d-flex justify-content-center py-2 my-2 text-light mx-auto">
-				<h1>Phone catalogue</h1>
+				<h1 className="hover-title">Phone catalogue</h1>
 			</div>
 		</Link>
 	);

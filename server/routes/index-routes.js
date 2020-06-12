8d75ae1264411c4c99aeaada0be50f22d5ac80b4
Víctor Ronda <victor.ronda@gmail.com>
2020-06-12 13:39:16 +0200

    dockerfile server working

--- server/routes/index-routes.js
+++ server/routes/index-routes.js
@@ -2,7 +2,18 @@ const { Router } = require('express');
 const router = Router();
 const DB = require('../phones.json');
 
-const phonesDB = DB.map(phone => ({...phone, imageFileName: `http://localhost:4000/images/${phone.imageFileName}`}))
+const phonesDB = DB.map(phone => ({
+    id: phone.id,
+    name: phone.name,
+    manufacturer: phone.manufacturer,
+    description: phone.description,
+    color: phone.color,
+    price: phone.price,
+    imageFileName: `http://localhost:4000/images/${phone.imageFileName}`,
+    screen: phone.screen,
+    processor: phone.processor,
+    ram: phone.ram
+}))
 
 router.get('/', (req, res) => {
     res.status(200).json(phonesDB);
